41aa81305f85401267cd556217e62e311e5a54cb
Justin Camerer <iamjwc@gmail.com>
2011-03-30 19:30:13 -0400

    Added contacts page, email messages to new sign ups, updated bio.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,15 +1,27 @@
 
 class ContactsController < ApplicationController
   def create
-    @contact = Contact.new(params[:contact])
-    if @contact.valid?
-      MailingListMailer.notify(@contact).deliver
-      @contact.save
+    @contact = Contact.find_by_email(params[:contact][:email])
+
+    if @contact
+      @contact.update_attributes(params[:contact])
     else
-      flash[:error] = "Invalid"
+      @contact = Contact.new(params[:contact])
+      if @contact.valid?
+        @contact.save
+
+        MailingListMailer.notify_admin(@contact).deliver
+        MailingListMailer.notify_user(@contact).deliver
+      else
+        flash[:error] = "Invalid Email"
+      end
     end
 
     redirect_to home_url
+  end
+
+  def index
+    @contacts = Contact.all
   end
 
 end
--- app/mailers/mailing_list_mailer.rb
+++ app/mailers/mailing_list_mailer.rb
@@ -1,14 +1,28 @@
 class MailingListMailer < ActionMailer::Base
   default :from => "thebirdhiveboys@gmail.com"
 
-  def notify(contact)
-    subject = if contact.is_street_team
-      "Street Team Subscription (#{[contact.name, contact.email].compact.join(" ")})"
+  def notify_admin(contact)
+    @contact = contact
+
+    if @contact.is_street_team
+      @subject = "Street Team Subscription (#{[@contact.name, @contact.email].compact.join(" ")})"
     else
-      "Mailing List Subscription (#{[contact.name, contact.email].compact.join(" ")})"
+      @subject = "Mailing List Subscription (#{[@contact.name, @contact.email].compact.join(" ")})"
     end
 
-    mail :to => "thebirdhiveboys+subscription@gmail.com", :subject => subject
+    mail :to => "thebirdhiveboys+subscription@gmail.com", :subject => @subject
+  end
+
+  def notify_user(contact)
+    @contact = contact
+
+    if @contact.is_street_team
+      @subject = "Thanks for joining the street team!"
+    else
+      @subject = "Thanks for joining our mailing list!"
+    end
+
+    mail :to => @contact.email, :subject => @subject
   end
 end
 
